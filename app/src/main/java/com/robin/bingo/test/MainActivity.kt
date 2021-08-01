d266572a435a2c54572dad46b14c9dedee0eb899
robin <531031061@qq.com>
2021-08-01 16:25:45 +0800

    ceshi

--- app/src/main/java/com/robin/bingo/test/MainActivity.kt
+++ app/src/main/java/com/robin/bingo/test/MainActivity.kt
@@ -2,10 +2,12 @@ package com.robin.bingo.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.robin.bingo.utils.Test
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Test().testa()
     }
 }
---
+++ bingolog/src/main/java/com/robin/bingo/log/LogTest.kt
@@ -0,0 +1,10 @@
+package com.robin.bingo.log
+
+import android.util.Log
+
+private const val TAG = "LogTest11"
+class LogTest {
+    fun testa(){
+        Log.i(TAG, "testa: ")
+    }
+}
